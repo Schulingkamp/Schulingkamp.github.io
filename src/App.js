9a643de66d6099fe67610f5ec908b7ebaf9158ed
Lee <leeschulingkamp@gmail.com>
2021-09-29 22:04:47 -0400

    Make changes to package.json, router for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navbar />
 
       <div className='main'>
