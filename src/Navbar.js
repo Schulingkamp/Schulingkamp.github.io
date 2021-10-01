98536766fa45574d26617adddd681a6d0e20e486
Lee <leeschulingkamp@gmail.com>
2021-09-30 23:28:37 -0400

    Move symbols above text on small screens

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,12 +9,25 @@ const Navbar = () => {
         <div className='myNavbar'>
             <img id='me' src={ avatarImage } alt='My headshot'></img>
             <ul className='myNavbarul'>
-                <Link to='/'><li className='myNavbarli'><i class="fas fa-home"></i> Home</li></Link>
-                <Link to='/about'><li className='myNavbarli'><i class="fas fa-address-card"></i> About</li></Link>
-                <Link to='projects'><li className='myNavbarli'> <i class="fas fa-project-diagram"></i> Projects</li></Link>
-                <a href={LinkedInLink} target='_blank' rel='noreferrer'><li className='myNavbarli'><i class="fab fa-linkedin"></i> LinkedIn</li></a>
-                <a href={GitHubLink} target='_blink' rel='noreferrer'><li className='myNavbarli'><i class="fab fa-github-square"></i> GitHub</li></a>
-                
+                <Link to='/'>
+                    <li className='myNavbarli'><i class="fas fa-home"></i> Home</li>
+                </Link>
+
+                <Link to='/about'>
+                    <li className='myNavbarli'><i class="fas fa-address-card"></i> About</li>
+                </Link>
+
+                <Link to='projects'>
+                    <li className='myNavbarli'><i class="fas fa-project-diagram"></i> Projects</li>
+                </Link>
+
+                <a href={LinkedInLink} target='_blank' rel='noreferrer'>
+                    <li className='myNavbarli'><i class="fab fa-linkedin"></i> LinkedIn</li>
+                </a>
+
+                <a href={GitHubLink} target='_blink' rel='noreferrer'><li className='myNavbarli'>
+                    <i class="fab fa-github-square"></i> GitHub</li>
+                </a>
             </ul>
         </div>
     );
